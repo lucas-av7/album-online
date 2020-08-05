443564ce669c9813fc152fdbf0ba70fe49572f3a
Lucas Vasconcelos <lucas.av7@yahoo.com>
2020-08-05 07:50:53 -0400

    Reset, variáveis e font-family CSS

--- src/App.vue
+++ src/App.vue
@@ -14,4 +14,24 @@ export default {
 </script>
 
 <style>
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+
+  :root {
+    --primary-color: #028FDF;
+    --secondary-color: #F6F9FC;
+    --primary-text-color: #FFF;
+    --secondary-text-color: #000;
+    --text-warning: #fc8600;
+  }
+
+  body {
+    background: linear-gradient(-45deg, #4CA8DA, #63AE99);
+    font-family: 'Roboto', sans-serif;
+    font-size: 62,5%;
+    color: var(--primary-text-color);
+  }
 </style>
