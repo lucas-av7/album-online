55a6ca7ff7eac8e08f3a713a5205172d297c3d5a
Lucas Vasconcelos <lucas.av7@yahoo.com>
2020-08-05 15:22:59 -0400

    Modal de editar e deletar album

--- src/views/AlbumView.vue
+++ src/views/AlbumView.vue
@@ -2,20 +2,62 @@
   <div class="albumView">
     <h1>Album name</h1>
     <MenuButton>
-      <p>Edit</p>
-      <p>Delete album</p>
+      <p @click="editAlbumModal = true">Edit</p>
+      <p @click="deleteAlbumModal = true">Delete album</p>
     </MenuButton>
     <Photos />
+
+    <Modal v-if="editAlbumModal" @clicked="editAlbumModal = false">
+      <h1>Edit album</h1>
+      <Forms>
+        <label for="title">Title</label>
+        <input type="text" name="title" placeholder="Album title">
+
+        <label for="description">Description</label>
+        <input type="text" name="description" placeholder="Album description">
+      </Forms>
+      <template slot="buttonsArea">
+      <ActionButton
+        @clicked="editAlbumModal = false"
+        text="Cancel"
+        type="secondary" />
+      <ActionButton
+        text="Save"
+        type="primary" />
+      </template>
+    </Modal>
+
+    <Modal v-if="deleteAlbumModal" @clicked="deleteAlbumModal = false">
+      <h1>Delete album?</h1>
+      <template slot="buttonsArea">
+      <ActionButton
+        @clicked="deleteAlbumModal = false"
+        text="Cancel"
+        type="secondary" />
+      <ActionButton
+        text="Delete"
+        type="danger" />
+      </template>
+    </Modal>
   </div>
 </template>
 
 <script>
 import Photos from '../components/Photos'
 import MenuButton from '../components/UI/MenuButton'
+import Modal from '../components/UI/Modal'
+import ActionButton from '../components/UI/ActionButton'
+import Forms from '../components/UI/Forms'
 
 export default {
   name: 'AlbumView',
-  components: { Photos, MenuButton }
+  components: { Photos, MenuButton, Modal, ActionButton, Forms },
+  data() {
+    return {
+      editAlbumModal: false,
+      deleteAlbumModal: false
+    }
+  }
 }
 </script>
 
