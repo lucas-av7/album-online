2efc7d1b5e8263528d514d22b6cfef507192fe71
Lucas Vasconcelos <lucas.av7@yahoo.com>
2020-08-06 10:51:43 -0400

    Regex para keywords

--- src/mixin/regexMixin.js
+++ src/mixin/regexMixin.js
@@ -2,6 +2,9 @@ export default {
   methods: {
     removeSpecialCharacters(string) {
       return string.replace(/[!@#$%^&*()_+\-=[\]{};'`:"\\|,.<>/?~]/i, '').replace(/  +/g, ' ')
+    },
+    removeSpecialCharactersKeywords(string) {
+      return string.replace(/[!@#$%^&*()_+\-=[\]{};'`:"\\|,.<>/?~ ]/i, '')
     }
   }
 }
