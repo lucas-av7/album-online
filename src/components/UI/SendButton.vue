79cef2af11230be2edf831273737826ba5c925f0
Lucas Vasconcelos <lucas.av7@yahoo.com>
2020-08-06 20:38:59 -0400

    Novas personalizações de botões

--- src/components/UI/SendButton.vue
+++ src/components/UI/SendButton.vue
@@ -1,10 +1,12 @@
 <template>
   <button class="sendButton"
+    :class="bg"
     :style="`width: ${width}px; height: ${height}px`"
     @click="$emit('clicked')">
     <i v-if="type =='plane'" class="fas fa-paper-plane"></i>
     <i v-if="type =='plus'" class="fas fa-plus"></i>
     <i v-if="type =='search'" class="fas fa-search"></i>
+    <i v-if="type =='down'" class="fas fa-arrow-down"></i>
   </button>
 </template>
 
@@ -15,6 +17,7 @@ export default {
     type: { type: String, default: 'plane' },
     width: { type: String, default: '60' },
     height: { type: String, default: '45' },
+    bg: { type: String, default: 'primary' },
   }
 }
 </script>
@@ -27,5 +30,15 @@ export default {
     background-color: var(--primary-color);
     color: var(--primary-text-color);
     font-size: 1.35rem;
+  }
+
+  .sendButton.primary {
+    background-color: var(--primary-color);
+    color: var(--primary-text-color);
+  }
+
+  .sendButton.secondary {
+    background-color: var(--secondary-color);
+    color: var(--secondary-text-color);
   }
 </style>
